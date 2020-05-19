e20080b7334ea3691d2dcd32d7edfcc70a1fbe9a
Amin Nerekar <AminN@citiustech.com>
2020-05-19 14:29:25 +0530

    Adding the project

---
+++ Asp.netCoreMVCCRUD/Asp.netCoreMVCCRUD/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCoreMVCCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage ="Dear CTZen you forgot to enter name.")] //ErrorMessage for custom message at view
+        [DisplayName("Full Name")] //This is display name on view
+        public string FullName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Emp. Code")]
+        public string EmpCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
